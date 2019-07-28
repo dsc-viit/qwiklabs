351a73de3415707ba4e44b2e210119ae6ff703f1
sampathbalivada <balivadask2000@gmail.com>
2019-07-29 01:31:20 +0530

    initial commit
    Prepping for Cloud Fest 2019

---
+++ main.js
@@ -0,0 +1,7 @@
+function redirect() {
+    setTimeout(()=>{
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeaod7fCTBXQIWq607Qtx4siwXS0oXYk-UFDP4DZWkL13LJGw/viewform?usp=sf_link";
+    }, 3000);
+}
+
+window.onload = redirect()
