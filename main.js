843b0215e9d2c73073c001f7882d460f0b8d405a
Sai Sampath Kumar <balivadask2000@gmail.com>
2019-09-05 20:18:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function redirect() {
     setTimeout(()=>{
-        window.location.href = "https://dscviit.tech";
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeZn-1tJYsZ6ZEGjUk1yoQcPdVQ-DjDUeU1Vcv59JNAX8FAXg/viewform?usp=sf_link";
     }, 3000);
 }
 
