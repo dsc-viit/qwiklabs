09dded8951c1f83905d995f86e4a2f541bd8816a
Sai Sampath Kumar <balivadask2000@gmail.com>
2019-10-01 19:23:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function redirect() {
     setTimeout(()=>{
-        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeZn-1tJYsZ6ZEGjUk1yoQcPdVQ-DjDUeU1Vcv59JNAX8FAXg/viewform?usp=sf_link";
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMs8i09sqZPdz-bFPl2rg77Tms3yuHNg8Amem19_mOYxZFEw/viewform?usp=sf_link";
     }, 3000);
 }
 
