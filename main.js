d5844c54afcad1ceaaf77207b6cae56f58372169
Sai Sampath Kumar <balivadask2000@gmail.com>
2019-10-04 10:53:25 +0530

    upadted to GCP Crash Course

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function redirect() {
     setTimeout(()=>{
-        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMs8i09sqZPdz-bFPl2rg77Tms3yuHNg8Amem19_mOYxZFEw/viewform?usp=sf_link";
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfknZUWZsuVwH3_klQqckzPx02mL6ye282K_6SjYxGaIQxnGw/viewform?usp=sf_link";
     }, 3000);
 }
 
