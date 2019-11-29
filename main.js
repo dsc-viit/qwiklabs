c4b64bd027e97574832cfb8a6de478488fb18959
Sai Sampath Kumar <balivadask2000@gmail.com>
2019-11-29 18:51:37 +0530

    Updated for Un-Hack me

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function redirect() {
     setTimeout(()=>{
-        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfknZUWZsuVwH3_klQqckzPx02mL6ye282K_6SjYxGaIQxnGw/viewform?usp=sf_link";
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeZn-1tJYsZ6ZEGjUk1yoQcPdVQ-DjDUeU1Vcv59JNAX8FAXg/viewform?usp=sf_link";
     }, 3000);
 }
 
